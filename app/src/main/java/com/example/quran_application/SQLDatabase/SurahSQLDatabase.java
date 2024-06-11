5cb7723627a3e0026f7a2573a24566e6b104d700
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-06-11 10:42:58 +0500

    SQL database  Quran_App commit

--- app/src/main/java/com/example/quran_application/SQLDatabase/SurahSQLDatabase.java
+++ app/src/main/java/com/example/quran_application/SQLDatabase/SurahSQLDatabase.java
@@ -1,4 +1,27 @@
 package com.example.quran_application.SQLDatabase;
 
-public class SurahSQLDatabase {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class SurahSQLDatabase extends SQLiteOpenHelper {
+
+    private String TABLE_NAME = "SURAH_TABLE";
+    private String DATABASE_NAME = "SURAH_DOWNLOADED_DATABASE";
+    private int DATABASE_VERSION = 1;
+    public SurahSQLDatabase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
